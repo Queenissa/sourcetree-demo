7853547764b99788eb7861bd805da17ab188f3e7
Queenissa <atacador.quency@gmail.com>
2020-05-20 16:10:13 +0800

    Code edited

--- calculator.py
+++ calculator.py
@@ -18,14 +18,14 @@ def multiply(num1, num2):
 def divide(num1, num2): 
     return num1 / num2 
   
-print("Please select operation -\n" \ 
-        "1. Add\n" \ 
-        "2. Subtract\n" \ 
-        "3. Multiply\n" \ 
+print("Please select operation: -\n"
+        "1. Add\n" 
+        "2. Subtract\n" 
+        "3. Multiply\n" 
         "4. Divide\n") 
 
 # Take input from the user  
-select = int(input("Select operations form 1, 2, 3, 4 :")) 
+select = int(input("Select operations form 1, 2, 3, 4:")) 
   
 number_1 = int(input("Enter first number: ")) 
 number_2 = int(input("Enter second number: ")) 
